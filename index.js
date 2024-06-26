b1113822c01100fc458053dedb69f91e9e795ccd
reidcook <90933623+reidcook@users.noreply.github.com>
2024-06-26 15:53:10 -0400

    fixed creating and changing users

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 root.render(<MyApp />)
 function MyApp(){
-    var [users, setUsers] = React.useState([{name: "Joe Shmo", age: "25", drugs: "Motrin", incident: "fell off bike"}]); // will hold all users
+    var [users, setUsers] = React.useState([{name: "Joe Shmo", age: "25", drugs: "Motrin", incident: "fell off bike"},{name: "Joess Shmo", age: "25", drugs: "Motrin", incident: "fell off bike"}]); // will hold all users
     var [user, setUser] = React.useState({name: "Joe Shmo", age: "25", drugs: "Motrin", incident: "fell off bike"}); // will hold current user
     var [screen, setScreen] = React.useState("Dashboard"); // will hold current screen
+    console.log(user)
     const changeScreen = (newScreen) => {
         setScreen(newScreen)
     }
@@ -20,7 +21,6 @@ function MyApp(){
         setUsers(newUsersList);
     }
     if(screen === "Dashboard"){
-        console.log(users)
         return(
             <div style={{backgroundColor: "#212120"}} className="container-sm d-flex flex-column h-100 border border-info-subtle border-5">
             <div className="row h-10"><Footer currentUser={user} setScreenProp={changeScreen} setUserProp={setUser}/></div>
@@ -112,46 +112,62 @@ class UserSelectScreen extends React.Component {
         this.userSetter = props.setUserProp;
         this.usersSetter = props.setUsersProp;
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleUserChange = this.handleUserChange.bind(this);
         this.state = {}
     }
     componentDidMount(){}
     handleSubmit(event){
         //this.userSetter({name: event.target[0]?.value, age: event.target[1]?.value, drugs: event.target[2]?.value, incident: event.target[3]?.value})
-        this.usersSetter({name: event.target[0]?.value, age: event.target[1]?.value, drugs: event.target[2]?.value, incident: event.target[3]?.value})
         event.preventDefault();
+        this.usersSetter({name: event.target[0]?.value, age: event.target[1]?.value, drugs: event.target[2]?.value, incident: event.target[3]?.value})
+        this.userSetter({name: event.target[0]?.value, age: event.target[1]?.value, drugs: event.target[2]?.value, incident: event.target[3]?.value})
+        this.screenSetter("Dashboard")
     }
     handleUserChange(event){
-        console.log(event)
-        this.userSetter(event.target.value)
+        // if(event.target.value === undefined){
+        //     console.log("undefined")
+        // }
+        // this.userSetter(event.target.value)
+        this.userSetter(this.allUsers.find(user => user.name === event.target.value))
+        this.screenSetter("Dashboard")
     }
     render(){
         return(
             <div style={{backgroundColor: "#212120"}} className="container-sm d-flex flex-column h-100 border border-info-subtle border-5">
                 <div className="row h-10 border-bottom border-light border-3"><Footer currentUser={this.user} setScreenProp={this.screenSetter}/></div>
-                <h1>Create a User</h1>
-                <form onSubmit={this.handleSubmit}>
-                    <label>
-                        Name:
-                        <input type="text" name="name" />
-                    </label>
-                    <label>
-                        Age:
-                        <input type="text" name="age" />
-                    </label>
-                    <label>
-                        Prescribed Drugs:
-                        <input type="text" name="drugs" />
-                    </label>
-                    <label>
-                        Incident:
-                        <input type="text" name="incident" />
-                    </label>
-                    <input type="submit" value="submit" />
-                </form>
-                <h1>Change User</h1>
-                <select onChange={this.handleUserChange}>
-                    {this.allUsers.map((user) => <option value={user}>{user.name}</option>)}
-                </select>
+                <div className="create-user">
+                    <h1>Create a User</h1>
+                    <form onSubmit={this.handleSubmit}>
+                        <label>
+                            Name: 
+                            <input type="text" name="name" />
+                        </label>
+                        <label>
+                            Age: 
+                            <input type="text" name="age" />
+                        </label>
+                        <label>
+                            Prescribed Drugs: 
+                            <input type="text" name="drugs" />
+                        </label>
+                        <label>
+                            Incident: 
+                            <input type="text" name="incident" />
+                        </label>
+                        <div style={{paddingTop: "1%"}}>
+                            <input className="submit-button" type="submit" value="Submit" />
+                        </div>
+                    </form>
+                </div>
+                <div className="change-user">
+                    <h1>Change User</h1>
+                    <select value={this.user.name} onChange={this.handleUserChange}>
+                        {this.allUsers.map((user) => <option style={{padding: "100px"}}value={user.name}>{user.name}</option>)}
+                    </select>
+                    <div style={{textAlign: "center", paddingTop: "20%"}}>
+                        <button className="home-button" onClick={() => this.screenSetter("Dashboard")}>Home</button>
+                    </div>
+                </div>
             </div>
         )
     }
